5aa91dd117cbaaed757f0f21cb331f4b4260788c
Sanjai Kumar <youremail@example.com>
2025-07-28 21:10:13 +0530

    Details Card Update

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -85,106 +85,117 @@ const MovieDetails = () => {
 
   
   return (
-    
-      <div className="model-top">
-        <div className="model-box">
-          <div className="content-box ">
-            <div className="head">
-              <div className="frame1">
-                <h1>
-                  {Title?.split(" ").slice(0, 6).join(" ")}
-                  {Title?.split(" ").length > 6 ? "..." : ""}
-                </h1>
-                <div className="frame 2 ">
-                  <div className="span ">
-                    {[Year, Runtime, Rated].join(" • ")}
-                  </div>
+    <div className="model-top">
+      <div className="model-box">
+        <div className="content-box ">
+          <div className="head">
+            <div className="frame1">
+              <h1>
+                {Title?.split(" ").slice(0, 6).join(" ")}
+                {Title?.split(" ").length > 6 ? "..." : ""}
+              </h1>
+              <div className="frame 2 ">
+                <div className="span ">
+                  {[Year, Runtime, Rated].join(" • ")}
                 </div>
-              </div>
-
-              <div className="frame3 ">
-                <button className="btn">
-                  {star_icon && (
-                    <img src={star_icon} alt="Star Icon" className="w-5 h-5" />
-                  )}
-                  {imdbRating}/10 (
-                  {`${Math.floor(
-                    parseInt(movie.imdbVotes.replace(/,/g, "")) / 10000
-                  )}k`}
-                  )
-                </button>
               </div>
             </div>
 
-            <div className="img-trailer">
-              <img src={Poster} alt={Title} />
+            <div className="frame3 ">
+              <button className="btn">
+                {star_icon && (
+                  <img src={star_icon} alt="Star Icon" className="w-5 h-5" />
+                )}
+                {imdbRating}/10 (
+                {`${Math.floor(
+                  parseInt(movie.imdbVotes.replace(/,/g, "")) / 10000
+                )}k`}
+                )
+              </button>
             </div>
-
-            {loading ? (
-              <Spinner />
-            ) : errorMessage ? (
-              <p className="text-red-500">{errorMessage}</p>
-            ) : (
-              <section className="info-section">
-                <div className="info">
-                  <div className="info-gener">
-                    <h2 className="gen">Generes</h2>{" "}
-                    <div className="gen-list ">
-                      {(Array.isArray(Genre) ? Genre : Genre.split(",")).map(
-                        (genre, idx) => (
-                          <span
-                            key={idx}
-                            className="px-2 py-1  text-white text-sm font-medium rounded-[6px]  bg-[#221F3D] justify-center"
-                          >
-                            {genre.trim()}
-                          </span>
-                        )
-                      ) || "N/A"}
-                    </div>
-                  </div>
-                  <div className="info-overview">
-                    <h2>Overview</h2>
-                    <p>{Plot || "N/A"}</p>
-                  </div>
-                  <div className="info-tagline">
-                    <h2>Type</h2>
-                    <p>{Type || "N/A"}</p>
-                  </div>
-                  <div className="info-release">
-                    <h2>Release date</h2>
-                    <p>{Released || "N/A"}</p>
-                  </div>
-                  <div className="info-country">
-                    <h2>Countries</h2>
-                    <p>{Country || "N/A"}</p>
-                  </div>
-                  <div className="info-actors">
-                    <h2>Actors</h2>
-                    <p>{Actors || "N/A"}</p>
-                  </div>
-                  <div className="info-actors">
-                    <h2>Director</h2>
-                    <p>{Director || "N/A"}</p>
-                  </div>
-                  <div className="info-language">
-                    <h2>BoxOffice</h2>
-                    <p>{BoxOffice || "N/A"}</p>
-                  </div>
-                  <div className="info-production">
-                    <h2>Production</h2>
-                    <p>{Production || "N/A"}</p>
-                  </div>
-                </div>
-
-                <button className={`visit-home ${animate ? 'animate-slide-out-left' : ''}`} onClick={handleVisitHome}>
-                  Visit Homepage <i className="bx bx-right-arrow-alt"></i>
-                </button>
-              </section>
-            )}
           </div>
+
+          <div className="img-trailer">
+            <img src={Poster} alt={Title} />
+          </div>
+
+          {loading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <section className="info-section">
+              <div className="info">
+                <div className="info-gener">
+                  <h2 className="gen">Generes</h2>{" "}
+                  <div className="gen-list ">
+                    {(Array.isArray(Genre) ? Genre : Genre.split(",")).map(
+                      (genre, idx) => (
+                        <span
+                          key={idx}
+                          className="px-2 py-1  text-white text-sm font-medium rounded-[6px]  bg-[#221F3D] justify-center"
+                        >
+                          {genre.trim()}
+                        </span>
+                      )
+                    ) || "N/A"}
+                  </div>
+                </div>
+                <div className="info-overview">
+                  <h2>Overview</h2>
+                  <p>{Plot || "N/A"}</p>
+                </div>
+                <div className="info-tagline">
+                  <h2>Type</h2>
+                  <p>{Type || "N/A"}</p>
+                </div>
+                <div className="info-release">
+                  <h2>Release date</h2>
+                  <p>{Released || "N/A"}</p>
+                </div>
+                <div className="info-country">
+                  <h2>Countries</h2>
+                  <p>{Country || "N/A"}</p>
+                </div>
+                <div className="info-actors">
+                  <h2>Actors</h2>
+                  <p>{Actors || "N/A"}</p>
+                </div>
+                <div className="info-actors">
+                  <h2>Director</h2>
+                  <p>{Director || "N/A"}</p>
+                </div>
+                <div className="info-actors">
+                  <h2>Language</h2>
+                  <p>{Language || "N/A"}</p>
+                </div>
+                <div className="info-awards">
+                  <h2>Awards</h2>
+                  <p>{Awards || "N/A"}</p>
+                </div>
+                <div className="info-language">
+                  <h2>BoxOffice</h2>
+                  <p>{BoxOffice || "N/A"}</p>
+                </div>
+                <div className="info-production">
+                  <h2>Production</h2>
+                  <p>{Production || "N/A"}</p>
+                </div>
+              </div>
+
+              <button
+                className={`visit-home ${
+                  animate ? "animate-slide-out-left" : ""
+                }`}
+                onClick={handleVisitHome}
+              >
+                Visit Homepage <i className="bx bx-right-arrow-alt"></i>
+              </button>
+            </section>
+          )}
         </div>
       </div>
-    
+    </div>
   );
 };
 
